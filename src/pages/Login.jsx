d9a84b2890d0fdf3bf5089ec4373498b9622b770
Emmanuel Murairi <emurairi18@alastudents.org>
2023-05-07 00:08:48 +0400

    registration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,27 @@
-import React, { useState } from 'react';
-import LoginForm from '../components/LoginForm';
-import SignUpForm from '../components/SignUpForm';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
+import React, { useState } from "react";
+import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUpForm";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function Login(props) {
-    const [animationParent] = useAutoAnimate();
+  const [animationParent] = useAutoAnimate();
 
-    const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-    return (
-        <div className='login w-screen h-screen flex'>
-
-            <div ref={animationParent} className={`login-container ${!isLogin && "login__isNotLogin"} flex`} >
-                {isLogin ? 
-
-                <LoginForm setIsLogin={setIsLogin}/>
-
-                :
-
-                <SignUpForm setIsLogin={setIsLogin}/>
-
-                }
-                
-
-                
-            </div>         
-            
-        </div>
-    );
+  return (
+    <div className="login w-screen h-screen flex">
+      <div
+        ref={animationParent}
+        className={`login-container ${!isLogin && "login__isNotLogin"} flex`}
+      >
+        {isLogin ? (
+          <LoginForm setIsLogin={setIsLogin} />
+        ) : (
+          <SignUpForm setIsLogin={setIsLogin} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/services/apis.js
+++ src/services/apis.js
@@ -1,2 +1,33 @@
+//const API_URL = "https://dominantdynast.maurice.webcup.hodi.host/api/";
+const API_URL = "http://localhost:8000/";
 
+export const login = (data) => {
+  //fetch
+  return fetch(API_URL + "accounts/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
 
+export const register = (data) => {
+  console.log(data);
+  return fetch(API_URL + "accounts/registration/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    console.log(response);
+    if (response.status === 204) {
+      return { status: "success" };
+    }
+  });
+};
