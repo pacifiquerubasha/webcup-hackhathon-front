66403fc464714aec1e826b8c17c924a1d1e8b75e
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2023-05-07 05:38:11 +0400

    Added page dream details

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "./pages/Home"
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/basic.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import UniqueBlog from './pages/UniqueBlog'
-import Contact from './pages/Contact'
+
+// import Home from "./pages/Home"
+// import UniqueBlog from './pages/UniqueBlog'
+// import Contact from './pages/Contact'
+// import UniqueDream from './pages/UniqueDream'
 // import AboutUs from './pages/AboutUs'
 // import Blog from './pages/Blog'
 // import Login from './pages/Login'
 // import MainApp from './pages/MainApp'
 // import DreamScience from './pages/DreamScience'
 
+const Home = React.lazy(() => import('./pages/Home'));
 const AboutUs = React.lazy(() => import('./pages/AboutUs'));
 const Blog = React.lazy(() => import('./pages/Blog'));
 const Login = React.lazy(() => import('./pages/Login'));
 const MainApp = React.lazy(() => import('./pages/MainApp'));
 const DreamScience = React.lazy(() => import('./pages/DreamScience'));
+const UniqueBlog = React.lazy(() => import('./pages/UniqueBlog'));
+const Contact = React.lazy(() => import('./pages/Contact'));
+const UniqueDream = React.lazy(() => import('./pages/UniqueDream'));
 
 function App() {
   useEffect(() => {
@@ -45,6 +51,7 @@ function App() {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/app" element={<MainApp/>} />
                 <Route path="/app/explain-dreams" element={<DreamScience/>} />
+                <Route path="/app/dream/:id" element={<UniqueDream/>} />
                 <Route path="*" element={<Home/>} />
 
               </Routes>        
---
+++ src/pages/UniqueDream.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function UniqueDream(props) {
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <>
+
+            <Header isUser={true}/>
+
+
+            {isLoading ? 
+
+            <div className='w-screen h-screen flex justify-center items-center'>
+                <div className='loader'></div>
+            </div>
+
+            :
+
+            <div className='unique__dream'>
+                <div className="container">
+                    <p className='dream__intro'>
+                        As I drifted off to sleep on [date], little did I know that my dreams would take
+                        me on a journey that felt like hours. When I finally woke up on [date] at [waking time],
+                        I was amazed by the vividness and clarity of the images that filled my mind.
+                    </p>
+
+                    <h5 className='text-gradient'>Here is my dream!</h5>
+                    <div className='dream__context'>
+                        Dream
+                    </div>
+
+                    <h5 className='text-gradient'>And Here Is What It Means</h5> 
+
+                    <div className='dream__assessment'>
+                        Assessment
+                    </div>
+
+                </div>
+            </div>
+
+            }
+            
+
+            
+
+            
+        </>
+    );
+}
+
+export default UniqueDream;
